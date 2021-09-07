75294122290e0a20fae37a5b223480f382350721
Kieran Gill <kieran.gill1997@gmail.com>
2021-09-07 18:47:30 -0400

    accept component

--- src/lib/components/FoldedPaperEffect.svelte
+++ src/lib/components/FoldedPaperEffect.svelte
@@ -1,29 +1,56 @@
 <script lang='ts'>
-	import { onMount } from "svelte";
+	import { onMount } from 'svelte';
+	import type { SvelteComponent } from 'svelte'
+
+	export let props: any
+	export let component: SvelteComponentConstructor<SvelteComponent, any>
 
 	let container: HTMLElement
+	let animationFinished = false
 
 	onMount(() => {
 		// @ts-ignore
-		const folded = new OriDomi(container,  {
+		let folded = new OriDomi(container,  {
 			vPanels:          5,    // number of panels when folding left or right (vertically oriented)
 			speed:            1200, // folding duration in ms
 			shadingIntensity: 3,    // lessen the shading effect
 			maxAngle:         40,   // keep the user's folds within a range of -40 to 40 degrees
+			touchEnabled: false,
 		});
 
-		folded.reveal(100);
-		
+		// container.childNodes.forEach(node => {
+		// 	node.eve
+		// })
+
+		folded.reveal(100)
+			.wait(1000)
+			.reveal(0)
+
 		setTimeout(() => {
-			folded.reveal(5)
-		}, (700 * Math.random()) + 1200)
+			folded.destroy(() => {
+				console.log('aesjasjfaskdfj;laskd')
+				animationFinished = true
+				folded = null
+			})
+		}, 1000)
 	})
 </script>
 
-<div class="container" bind:this={container}>
-	<slot />
-</div>
+<!-- Rerender after animation finishes so that event listeners are re initialized -->
+{#if animationFinished == false}
+	<div class='container' bind:this={container}>
+		<svelte:component this={component} {...props} />
+	</div>
+{:else}
+	<div class='container' bind:this={container}>
+		<svelte:component
+		on:click={e => console.log(e)} 
+		this={component} {...props} />
+	</div>
+{/if}
 
 <style>
-
+	.container {
+		margin: var(--margin);
+	}
 </style>
