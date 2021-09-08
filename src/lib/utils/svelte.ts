c4e0484ed0f7c96f8e62dc60e2e6687a7902d990
Kieran Gill <kieran.gill1997@gmail.com>
2021-09-08 14:59:05 -0400

    single animation context so that we dont requestAnimFrame in more than one location

--- src/lib/utils/svelte.ts
+++ src/lib/utils/svelte.ts
@@ -1,3 +1,4 @@
+import { getContext, setContext } from 'svelte';
 import type { Writable } from 'svelte/store';
 import { writable } from 'svelte/store';
 
@@ -10,4 +11,38 @@ export const persistent = <T>(key: string, initValue: T): Writable<T> => {
         localStorage.setItem(key, JSON.stringify(val));
     })
     return store;
+}
+
+export const getAnimationContext = (): { register: (name: string, fn: (time: DOMHighResTimeStamp) => void) => void } => {
+    return getContext('animation')
+}
+
+export const setAnimationContext = () => {
+    const context = createAnimationContext()
+
+    setContext('animation', context)
+
+    return context.animationId
+}
+
+const createAnimationContext = () => {
+    const animationCallbacks: Record<string, (time?: DOMHighResTimeStamp) => void> = {}
+    const animationId = writable(null)
+
+    const renderAnimations = (time: DOMHighResTimeStamp) => {
+        Object.values(animationCallbacks).forEach(fn => {
+            fn(time)
+        })
+
+        animationId.set(requestAnimationFrame(renderAnimations))
+    }
+
+    animationId.set(requestAnimationFrame(renderAnimations))
+
+    return {
+        animationId,
+        register: (name: string, fn: (time?: DOMHighResTimeStamp) => void) => {
+            animationCallbacks[name] = fn
+        },
+    }
 }
